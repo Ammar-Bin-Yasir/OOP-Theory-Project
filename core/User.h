0c01b00acb2ba65e98c7cc037cceb12d4f133f3a
Ammar Bin Yasir <ammarbinyasir2007@gmail.com>
2026-05-30 02:47:11 +0500

    Finalized the header file for the Abstract User class

---
+++ core/User.h
@@ -0,0 +1,35 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+// Forward declaration to avoid circular dependency
+class TransportManager; 
+
+class User 
+{
+    private:
+        std::string username, password, role;
+    
+    // Base classes will inherit these
+    public:
+        // Constructors
+        User(const std::string& username, const std::string& password, const std::string& role);
+        
+        // Destructor
+        virtual ~User() = default;
+
+        // These are inherited by both Admin and Passenger so both can access but cannot modify
+        std::string getUsername() const;
+        std::string getRole() const;
+
+        // setter for password with validation
+        void changePassword(const std::string& oldPassword, const std::string& newPassword);
+        bool checkPassword(const std::string& input) const;
+        
+    
+        // Pure Virtual function to be implemented by derived classes
+        virtual void showMenu(TransportManager& manager) = 0;
+};
+
+#endif 
