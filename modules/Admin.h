366f135fba2d8981488c42dcbeb8610ab3c0f247
Ammar Bin Yasir <ammarbinyasir2007@gmail.com>
2026-05-30 03:03:25 +0500

    Fanilized the draft for the Admin Derived Class

---
+++ modules/Admin.h
@@ -0,0 +1,21 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "core/User.h"
+
+class TransportManager; 
+class Admin : public User 
+{
+    public:
+        // Constructor
+        Admin(const std::string& username, const std::string& password);
+
+        // Destructor
+        ~Admin() override = default;
+
+        // Override the pure virtual function to render the administrative operations
+        void showMenu(TransportManager& manager) override;
+
+};
+
+#endif 
