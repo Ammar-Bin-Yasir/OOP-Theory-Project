6510b01055146d37e20d586791df7d32840c2dac
Ammar Bin Yasir <ammarbinyasir2007@gmail.com>
2026-05-30 04:43:34 +0500

    Added the header file code for the Route class

---
+++ modules/Route.h
@@ -0,0 +1,36 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+
+#include <string> 
+
+class Route 
+{
+    private:
+        std::string routeID, startPoint, endPoint, routeDetails;
+        double distance;
+
+    public:
+        // Constructors
+        Route();
+        
+        Route(const std::string& id, const std::string& start, const std::string& end, double dist, const std::string& details);
+
+        // For reading from files
+        Route(std::string csvLine); 
+
+        // Getters (Essential for Manager Cross-Referencing)
+        std::string getRouteID() const;
+        double getDistance() const;
+        std::string getStartPoint() const;
+        std::string getEndPoint() const;
+        std::string getRouteDetails() const; 
+
+        // Display and Serialization Methods
+        void displayRoute() const;
+        std::string toCSV() const;
+
+        // Operator Overloading
+        bool operator==(const Route& other) const;
+};
+
+#endif
