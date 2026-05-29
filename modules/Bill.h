88884aa938c7a4e2384ec61e8c0e6e714263148c
Ammar Bin Yasir <ammarbinyasir2007@gmail.com>
2026-05-30 03:57:00 +0500

    Added the header file code for the Bill Class

---
+++ modules/Bill.h
@@ -0,0 +1,42 @@
+/* Each bill will be composed inside of TransportPass class which is associated with each passenger. 
+    The bill does not exist independently; it calculates the monthly bill for the registered passenger. */
+    
+#ifndef BILL_H
+#define BILL_H
+
+#include <string>
+
+class Bill {
+private:
+    double monthlyFee;
+    double lateFine;
+    bool isPaid;
+    std::string dueDate;
+
+public:
+    // 1. Default Constructor (Initializes blank fields for safety)
+    Bill();
+
+    // 2. Standard Interactive Constructor (Used when an Admin creates a new pass request)
+    Bill(double fee, std::string due);
+
+    // 3. Complete Reconstruction Constructor (Crucial for File Loading without data loss)
+    Bill(double fee, double fine, std::string due, bool paidStatus);
+
+    
+    ~Bill() = default;
+
+    // Getters (Essential for Pass-Level Serialization and Engine Reporting)
+    double getMonthlyFee() const;
+    double getLateFine() const;
+    double getTotalAmount() const; // Returns monthlyFee + lateFine
+    bool getIsPaid() const;
+    std::string getDueDate() const;
+
+    // Business Logic Methods
+    void markAsPaid();
+    void applyLateFine(double fineAmount);
+    void displayBill() const;
+};
+
+#endif
